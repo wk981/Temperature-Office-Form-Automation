4a714c365aded1ac457ac48e8bcb3f10fcacb7aa
wk981 <kwaggin@gmail.com>
2022-04-23 12:47:10 +0800

    New updates

--- Automate_fill_form.py
+++ Automate_fill_form.py
@@ -9,6 +9,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+#Function for interacting with web interfaces
+def health_travel(temperature):
+    RadioButtonClick = web.find_element_by_css_selector("input[type='radio'][value='Value']").click() 
+    t.sleep(4)
+
+    textbox = web.find_element_by_css_selector('input[aria-labelledby="QuestionId_re8865ca7664940f58f276cd4834dfcf6 QuestionInfo_re8865ca7664940f58f276cd4834dfcf6"]')
+    textbox.send_keys(temperature)
+
+    web.save_screenshot('./screenshot/submitted1.png')
+    web.quit()#Important! Remeber to exit to prevent any memory leak.
+
+#Options for web browsing
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-extensions")
 options.add_argument("--profile-directory=Default")
@@ -18,21 +30,13 @@ options.add_argument("--no-sandbox")
 options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36")
 #options.add_argument("headless")
 
+#Initialising the webdriver
 #web = webdriver.Phantomjs()
 web = webdriver.Chrome(chrome_options=options)
 web.execute_script("Object.defineProperty(navigator, 'web', {get: () => undefined})")
 web.delete_all_cookies()
 web.get('') #replace with website
 timeout = 5
-
-def health_travel(temperature):
-    RadioButtonClick = web.find_element_by_css_selector("input[type='radio'][value='Value']").click()
-    t.sleep(4)
-    textbox = web.find_element_by_css_selector('input[aria-labelledby="QuestionId_re8865ca7664940f58f276cd4834dfcf6 QuestionInfo_re8865ca7664940f58f276cd4834dfcf6"]')
-    textbox.send_keys(temperature)
-
-    web.save_screenshot('./screenshot/submitted1.png')
-    web.quit()
 
 try:
     element_present = EC.presence_of_element_located((By.ID, 'meridies-input'))
